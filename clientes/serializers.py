36f1d951d84dfaa3d345f03ef758c654919a164e
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-01-22 14:26:32 -0300

    Video_3.3

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from clientes.models import Cliente
-from clientes.validators import cpf_valido,nome_valido,rg_valido
+from clientes.validators import cpf_valido,nome_valido,rg_valido,celular_valido
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,14 +9,13 @@ class ClienteSerializer(serializers.ModelSerializer):
     
     def validate(self, data):
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({'cpf':"O CPF deve ter 11 dígitos"})
+            raise serializers.ValidationError({'cpf':"Número de CPF inválido"})
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome':"Não inclua números neste campo"})
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'rg':"O RG deve ter 9 dígitos"})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':"O número de celular deve seguir este modelo: 11 91234-1234 (respeitando os espaços e traço)"})
         return data
-    # def validate_celular(self, celular):
-    #     if len(celular) < 11:
-    #         raise serializers.ValidationError("O celular deve ter 11 dígitos")
-    #     return celular
+
     
